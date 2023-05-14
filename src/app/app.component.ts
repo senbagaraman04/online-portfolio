047fc04c25df54ce0ab3e01d9caad8c17b82921a
senbagaraman04 <senbagaraman04@gmail.com>
2023-05-14 21:29:26 +0530

    fix: updated contents for homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'online-portfolio';
+
+  
 }
+
+/**
+ * 
+ * Creating an online portfolio can help you establish a professional online presence and showcase your work in a polished way¹³. It can also become a source of inspiration for other creative-minded people². Here are some benefits of creating an online portfolio:
+
+1. Professional way to showcase your work: Building a website about your brand and experience is a polished way to share your expertise with others¹.
+2. Great first impression for employers: An online portfolio provides a visual and verbal example of the level of dedication you put into your craft².
+3. Increases your visibility and online presence: When an employer Googles your name, your professional portfolio will be one of the first search results that pops up³.
+4. Shows you’re more than just a resume: Because of the flexibility of an online portfolio, you’re able to show your personality by choosing design, layout and the copy you use³.
+5. Flexibility: Unlike other platforms, a website allows you to create a specific image and establish your brand².
+
+I hope this helps!
+
+Source: Conversation with Bing, 14/5/2023
+(1) 5 Benefits Of Creating An Online Portfolio - Work It Daily. https://www.workitdaily.com/creating-online-portfolio.
+(2) 5 Benefits Of Creating An Online Portfolio - University of Adelaide. https://careerhub.adelaide.edu.au/students/news/detail/361/5-benefits-of-creating-online-.
+(3) Create an Online Portfolio: Benefits and Building Tips - UCraft. https://www.ucraft.com/blog/the-benefits-of-an-online-portfolio.
+ */
+
+
+/**
+ * 
+ * Hiring creators from portfolio creation websites can be beneficial for clients in many ways. Here are some advantages of hiring creators from such websites:
+
+1. Save time, money, and resources: Hiring a content creator can save you time and money by providing you with high-quality content that meets your needs⁴.
+2. A la carte options, built to scale: The best part of hiring a content creator is your a la carte opportunities. You can choose the services you need and scale up or down as needed⁴.
+3. Provides a platform to showcase your relevant skills and experience: A portfolio website provides a platform to showcase your relevant skills and experience³.
+4. Shows your personality: A portfolio website lets clients see your personality and get a sense of who you are as a creator³.
+5. Lets hiring managers find you instead of you reaching out to them: Having a portfolio website makes it easier for hiring managers to find you online³.
+
+I hope this helps!
+
+Source: Conversation with Bing, 14/5/2023
+(1) 5 Major Benefits of Hiring a Content Creator for Your Website. https://www.iwriter.com/blog/5-major-benefits-of-hiring-a-content-creator-for-your-website/.
+(2) How to Create a Portfolio Website Using HTML, CSS, JavaScript, and .... https://www.freecodecamp.org/news/how-to-create-a-portfolio-website-using-html-css-javascript-and-bootstrap/.
+(3) 12 Essential Tips to Making Your Portfolio Website Stand Out. https://elementor.com/blog/portfolio-website/.
+(4) How to create a portfolio - W3Schools. https://www.w3schools.com/howto/howto_website_create_portfolio.asp.
+(5) Why a portfolio is important for your career | Career | Capella .... https://www.capella.edu/blog/career/why-a-portfolio-is-important-for-your-career/.
+(6) How To Make an Interview Portfolio (And Why It's Important). https://www.indeed.com/career-advice/interviewing/portfolio-interview.
+ */
+
+
+/**
+ * 
+ * Clients should choose portfolio creation websites for their hiring needs because:
+
+1. A portfolio website presents your professionalism and dedication to the craft better and more clearly than any CV¹.
+2. An online portfolio is certain to make you available and searchable for new clients. It lets your potential employers find you the moment they get round to using a search engine or looking through candidates’ applications¹.
+3. A portfolio published on the web humanizes you as a master of the craft¹.
+4. Creating a portfolio website is the best way to show off your work and attract new clients².
+5. A well-designed website will provide potential clients with a clear overview of your skills and experience, as well as examples of your past work².
+6. Creating a portfolio website is a great way to stay innovative. By sharing your work with others, you can receive feedback and peer review that will help you improve your skills².
+
+I hope this helps!
+
+Source: Conversation with Bing, 14/5/2023
+(1) Why Do You Need a Portfolio Website and How to Create it? - Weblium. https://weblium.com/blog/why-do-you-need-a-portfolio-website-and-how-to-create-it/.
+(2) 8 Reasons Why You Need a Portfolio Website. https://www.bbndry.com/blog/8-reasons-why-you-need-a-portfolio-website.
+(3) 10 Reasons You Should Build a Portfolio Website - Exprance. https://www.exprance.com/10-reasons-you-should-build-a-portfolio-website/.
+ */
