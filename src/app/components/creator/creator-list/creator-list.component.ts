54b99ab37e1a5454a65d4f4dd686a2a13b100699
senbagaraman04 <senbagaraman04@gmail.com>
2023-05-19 19:45:50 +0530

    fix: updated error messages

--- src/app/components/creator/creator-list/creator-list.component.ts
+++ src/app/components/creator/creator-list/creator-list.component.ts
@@ -1,24 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CreatorProfile } from '../../../service/CreatorProfile';
 import { CreatorlistService } from 'src/app/service/creatorlist.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-creator-list',
   templateUrl: './creator-list.component.html',
   styleUrls: ['./creator-list.component.scss']
 })
-export class CreatorListComponent  implements OnInit {
+export class CreatorListComponent implements OnInit {
   items: CreatorProfile[] = [];
+  creatorForm!: FormGroup;
   constructor(private creatorListService: CreatorlistService) { }
- 
+
 
   ngOnInit(): void {
+    this.creatorForm = new FormGroup({ searchVar: new FormControl('',) });
+
+
     this.pullCreators();
+
+    this.getFormControl('searchVar').valueChanges.subscribe((data: string) => {
+      this.pullCreators();
+      if (data !== '') {
+        this.items = this.items.filter(x => {
+          return x.name.toLocaleLowerCase().includes(data.toLocaleLowerCase())
+        })
+      } else {
+        this.pullCreators();
+      }
+    });
   }
+
   pullCreators() {
-    
-       this.items = this.creatorListService.getCreatorList()
-     
+    this.items = this.creatorListService.getCreatorList();
+  }
+
+  getFormControl(fieldName: string): FormControl {
+    return this.creatorForm.get(fieldName) as FormControl;
   }
 
 }
