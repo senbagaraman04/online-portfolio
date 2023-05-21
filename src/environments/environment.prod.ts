74553d051ae6e1fe1fb7859ed4a0a1aea7b64f6a
senbagaraman04 <senbagaraman04@gmail.com>
2023-05-21 11:32:11 +0530

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  appwriteurl: 'https://cloud.appwrite.io/v1',
-  projectID: '646062dd9e9c1e54e556',
-  databaseID: '646799e844731b12352a',
-  collectionID: '646799f1d1da44dbd798'
-};
