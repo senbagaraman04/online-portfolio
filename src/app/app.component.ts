accf03041d55248f0e25858d474410cd5e04b1c7
senbagaraman04 <senbagaraman04@gmail.com>
2023-05-21 09:13:33 +0530

    fix: added router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'online-portfolio';
+  showHeader = true;
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      console.group(router.url)
+      if (router.url.startsWith('/details')) {
+       this.showHeader = false;
+      }
+      else{
+        this.showHeader = true;
+      }
+    })
+  }
+
 }
  
